888481ca5f539a0e7c49f1af4535f5e0ccbad401
Eksuzyan <vita.eksuzyan@bk.ru>
2023-04-17 11:30:01 +0300

    settings reducer test(settings open must change)

--- src/redux/settingsReducer.test.ts
+++ src/redux/settingsReducer.test.ts
@@ -1,5 +1,5 @@
 import { SettingsState } from "../../types";
-import { settingsReducer, ChangeFieldSize, changeFieldSize } from "./settingsReducer";
+import { settingsReducer, ChangeFieldSize, changeFieldSize, changeSettingsOpen, ChangeSettingsOpen } from "./settingsReducer";
 
 const state: SettingsState = {
     fieldWidth: 3,
@@ -12,8 +12,20 @@ const state: SettingsState = {
 describe('field width and field height must change', () => {
     it('field width must be 4, field height must be 4', () => {
         const action: ChangeFieldSize = changeFieldSize();
+
         const result = settingsReducer(state, action);
+
         expect(result.fieldWidth).toBe(4);
         expect(result.fieldHeight).toBe(4);
+    });
+});
+
+describe('settings open must change', () => {
+    it('settings open must be true', () => {
+        const action: ChangeSettingsOpen = changeSettingsOpen(true);
+
+        const result = settingsReducer(state, action);
+
+        expect(result.settingsOpen).toBe(true);
     });
 });
