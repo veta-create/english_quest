80c6b961be5e433cbf7240724ae0fd81ea38f23e
Eksuzyan <vita.eksuzyan@bk.ru>
2023-04-26 12:11:07 +0300

    backend

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// запросы от веб-сайта, который хостится в одном домене, к серверу в другом домене
+app.use(cors());
+app.use(express.json());
+
+app.get('/api', async function (req, res) {
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("fieldsdb");
+        const collection = db.collection("fields");
+        const results = await collection.find().toArray();
+        res.send(results);
+        return results;
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    };
+});
+
+app.post('/api', async function (req, res) {
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("fieldsdb");
+        const collection = db.collection("fields");
+        await collection.insertOne(req.body);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    };
+});
+
+app.listen(PORT, () => {
+    console.log("Server started on PORT " + PORT);
+});
